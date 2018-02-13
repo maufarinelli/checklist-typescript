9acdf4a9ef0ea0cbdeb18902d3192d8c2d7c1d7c
Mauricio Farinelli <mauricio@sweetiq.com>
2018-02-13 12:22:06 -0500

    Add prompt

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -1,19 +1,53 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import {Link, Prompt, withRouter} from 'react-router-dom';
 import './header.css';
 
-const Header = () => (
-    <header>
-        <nav>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                {' | '}
-                <li><Link to="/checklist">Add CheckList</Link></li>
-                {' | '}
-                <li><Link to="/list">See All</Link></li>
-            </ul>
-        </nav>
-    </header>
-);
+class Header extends React.Component<any, any> {
+    constructor(props: any) {
+        super(props);
 
-export default Header;
+        this.state = {
+            shouldShowPrompt: false
+        };
+    }
+
+    componentDidMount() {
+        this.handlePrompt();
+    }
+
+    componentDidUpdate(prevProps: any) {
+        if (this.props.location !== prevProps.location) {
+            this.handlePrompt();
+        }
+    }
+
+    handlePrompt() {
+        const CHECKLIST_PATH = '/checklist';
+        const LIST_PATH = '/list';
+        const HOME_PATH = '/';
+
+        if (this.props.location.pathname === HOME_PATH || this.props.location.pathname === LIST_PATH) {
+            this.setState({shouldShowPrompt: false});
+        } else if (this.props.location.pathname === CHECKLIST_PATH) {
+            this.setState({shouldShowPrompt: true});
+        }
+    }
+
+    render() {
+        return (<header>
+            <nav>
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/checklist">Add CheckList</Link></li>
+                    <li><Link to="/list">See All</Link></li>
+                </ul>
+            </nav>
+            <Prompt
+                when={this.state.shouldShowPrompt}
+                message={'You did not save this checklist yet. If you leave this page, you loose everything. Are you sure you want to leave?'}
+            />
+        </header>);
+    }
+}
+
+export default withRouter(Header);
